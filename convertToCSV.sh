0aaf757f204a635cab1f14935632ad0d7c27966f
Aryan Yadav <aryan.yadav@gameskraft.com>
2023-09-05 13:01:32 +0530

    Adding files to branch

---
+++ convertToCSV.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p "Enter the starting date for your logs: " start
+read -p "Enter the ending date for your logs: " end
+
+fields=".user_id, .display_name, .mobile, .email"
+
+json_to_csv() {
+    local input_file="$1"
+    local csv_file="$2"
+    jq -r "[$fields] | @csv" "$input_file" >> "$csv_file"
+}
+
+for file in ./whale*
+do
+    cur_date=$(echo $file | cut -d "_" -f 4)
+    output_file="output_${cur_date}.csv"
+    date=$(echo $cur_date | cut -d "-" -f 2)
+    
+    if [ $date -lt $start -o $date -gt $end ]; then
+        continue
+    fi
+    echo "userId,displayName,mobile,email" > "$output_file"
+    json_to_csv "$file" "$output_file"
+done
+echo "Conversion done !"
